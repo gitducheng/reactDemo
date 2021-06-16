87f8def0b3dd969b9d8250d16addd019a5cb8a62
gitducheng <1147914778@qq.com>
2021-06-16 21:05:58 +0800

    feature:补充评价部分，尝试ts语法的不同写法

--- src/components/OrderItem/index.tsx
+++ src/components/OrderItem/index.tsx
@@ -9,10 +9,22 @@ type Props = {
         price: string
     }
 }
-export default class OrderItem extends Component<Props> {
+interface State {
+    isEditing: boolean
+}
+export default class OrderItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.handleCannelComment = this.handleCannelComment.bind(this)
+        this.state = {
+            isEditing: false
+        }
+    }
+   
     render() {
         const { product, detail, price } = this.props.data
         return (
+            <div>
             <div className="orderItem">
                 <div className="orderItem_picContext">
                     <img src={img} alt=""/>
@@ -23,9 +35,46 @@ export default class OrderItem extends Component<Props> {
                     <p>price：{price}</p>
                 </div>
                 <div className="orderItem_button">
-                    <button>评价</button>
+                    <button onClick={this.handleOpenComment}>评价</button>
                 </div>
             </div>
+            { this.state.isEditing? this.renderComment(): null }
+            </div>
         )
+    }
+
+    renderComment() {
+        return (
+            <div>
+                <textarea cols={30} rows={10}></textarea>
+                {this.renderStars()}
+                <button>提交</button>
+                <button onClick={this.handleCannelComment}>取消</button>
+            </div>
+        )
+    }
+
+    renderStars() {
+        return (
+            <div>
+                {
+                    [1,2,3,4,5].map((item, index) => {
+                        return <span key={index}>★</span>
+                    })
+                }
+            </div>
+        )
+    }
+
+    handleOpenComment = () => {
+        this.setState({
+            isEditing: true
+        })
+    }
+
+    handleCannelComment() {
+        this.setState({
+            isEditing: false
+        })
     }
 }
